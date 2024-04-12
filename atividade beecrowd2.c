44fbdbc5526a556ba90a3401104503333c4e76b2
ricardonerys <150439161+ricardonerys@users.noreply.github.com>
2024-04-12 00:32:05 +0000

    beecrowd2

---
+++ atividade beecrowd2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+ 
+
+int main(){
+    int MATRIZSIZE=0;
+    scanf("%d",&MATRIZSIZE);
+    int matriz[MATRIZSIZE-1][MATRIZSIZE-1];
+
+    for(int i=0;i<MATRIZSIZE;i++){
+        for(int j=0;j<MATRIZSIZE;j++){
+            
+            if(i==j){
+            matriz[i][j]=1;
+            }else if(i+j==MATRIZSIZE-1){
+            matriz[i][j]=2;
+            }else 
+            matriz[i][j]=3;
+        }   
+    }
+
+    for(int i=0;i<MATRIZSIZE;i++){
+        for(int j=0;j<MATRIZSIZE;j++){
+        printf("%d",matriz[i][j]);
+        }
+    printf("\n");
+    }
+    return 0;
+}
